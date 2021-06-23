f32ca736d07eb3f37707ece1d05c80677be5cba6
Nick-McGinnis <nickmcginnis17@gmail.com>
2021-06-22 21:49:59 -0400

    City Name & Icon Display
    Displays the city name "Cupertino, CA"
    Used SF Symbols to get Weather icon
    Added temp label
    
    **Values are hardcoded for as this project is to understand the basics of SwiftUI**

--- Learn-SwiftUI-Weather-App/ContentView.swift
+++ Learn-SwiftUI-Weather-App/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		ZStack{
+			LinearGradient(gradient: Gradient(colors: [.green, .blue]),
+						   startPoint: .topLeading,
+						   endPoint: .bottomTrailing)
+				.edgesIgnoringSafeArea(.all)
+			VStack {
+				Text("Cupertino, CA")
+					.font(.system(size: 32, weight: .medium, design: .default))
+					.foregroundColor(.white)
+					.padding()
+				VStack(spacing: 10) {
+					Image(systemName: "cloud.sun.fill")
+						.renderingMode(.original)
+						.resizable()
+						.aspectRatio(contentMode: .fit)
+						.frame(width: 180, height: 180, alignment: .center)
+					Text("76°")
+						.font(.system(size: 70, weight: .medium, design: .default))
+						.foregroundColor(.white)
+				}
+				Spacer()
+			}
+		}
     }
 }
 
