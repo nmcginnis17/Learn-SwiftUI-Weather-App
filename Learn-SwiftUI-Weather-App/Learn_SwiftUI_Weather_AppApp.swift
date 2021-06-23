fc97d82511025707e0889e0480d843cc244d6e99
Nick-McGinnis <nickmcginnis17@gmail.com>
2021-06-22 21:19:46 -0400

    initial commit

---
+++ Learn-SwiftUI-Weather-App/Learn_SwiftUI_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_SwiftUI_Weather_AppApp.swift
+//  Learn-SwiftUI-Weather-App
+//
+//  Created by Nicholas McGinnis on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Learn_SwiftUI_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
